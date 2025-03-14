2ed3aa7c427aee1978b982c50554d8e2d96d4f90
Dmitriy Myasnikov <myasnikov.qubership@gmail.com>
2025-03-14 21:03:48 +0300

    feat(ci): improve nifi autotests

--- .github/workflows/sh/nifi-lib.sh
+++ .github/workflows/sh/nifi-lib.sh
@@ -37,8 +37,16 @@ wait_for_nifi(){
     res=1
     while [ "$res" != "0" ]; do
         echo "Waiting for nifi to be available under URL = $nifiUrl, remaining time = $remainingTime"
-        curl -sS --connect-timeout 5 --max-time 10 "$nifiUrl" || {res="1"; echo "Request failed, continue waiting..."; }
-        res=$?
+        res=0
+        resp_code=""
+        resp_code=$(curl -sS -w '%{response_code}' -o ./temp-resp.json --connect-timeout 5 --max-time 10 "$nifiUrl") || { res="$?"; echo "Failed to call NiFi API, continue waiting..."; }
+        if [ "$res" == "0" ]; then
+            if [ "$resp_code" != '200' ]; then
+                res='$resp_code'
+                echo "Got response with code = $resp_code and body: "
+                cat ./temp-resp.json
+            fi
+        fi
         echo ""
         currentTime=$(date +%s)
         remainingTime=$((endTime-currentTime))
